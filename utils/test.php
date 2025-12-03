0c1cf5cd1ee47b00e52a9ffe7d8146cc5c85456f
Lothar Arbaiza <lothararbaiza0506@gmail.com>
2025-12-03 18:17:28 -0500

    Limpieza de archivos temporales y documentación de desarrollo

--- utils/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "PHP está funcionando correctamente<br>";
-echo "Directorio actual: " . __DIR__ . "<br>";
-echo "Archivo index.php existe: " . (file_exists('index.php') ? 'SÍ' : 'NO') . "<br>";
-echo "CodeIgniter system existe: " . (is_dir('system') ? 'SÍ' : 'NO') . "<br>";
-echo "Application existe: " . (is_dir('application') ? 'SÍ' : 'NO') . "<br>";
-phpinfo();
-?>
